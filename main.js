15cd0335951b5be7c9d6bc9c7b87c38feecd90a1
Elina Jaimes <ev.jaimes@uniandes.edu.co>
2021-02-27 23:54:06 -0500

    Entrega final

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//Autor: Elina Jaimes, 201816315
+
 //Leer archivos facilito. FECTH ES UNA PROMESA, entonces debemos resolverla
 const URL =
   "https://gist.githubusercontent.com/josejbocanegra/b1873c6b7e732144355bb1627b6895ed/raw/d91df4c8093c23c41dce6292d5c1ffce0f01a68b/newDatalog.json";
